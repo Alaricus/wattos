25622d2229021a907bb50c6758fff124af77d63c
Alaric von Teplitz <alaric2000@gmail.com>
2019-02-12 19:39:07 -0700

    Fetching data.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [ships, setShips] = useState([]);
+
+  useEffect(() => {
+    getShipData();
+  }, []);
+
+  const getShipData = async () => {
+    try {
+      const response = await fetch('https://demo7475333.mockable.io/spaceships');
+      if (response.ok) {
+        const result = await response.json();
+        setShips(result.products);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <p>Watto’s Spaceship Emporium</p>
+      <ol>
+        {
+          ships && ships.map(ship => (<li key={ship.name}>{ship.name}</li>))
+        }
+      </ol>
+    </div>
+  );
+};
 
 export default App;
