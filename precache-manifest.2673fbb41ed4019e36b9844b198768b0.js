3e0ccdf1b5356f747eac9e35255c5872b3a1ab04
Alaric von Teplitz <alaric2000@gmail.com>
2019-03-18 17:31:34 -0600

    Updates

---
+++ precache-manifest.2673fbb41ed4019e36b9844b198768b0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "0166caf1ebef1c945e693f490dbf1c7a",
+    "url": "/wattos/static/media/yqoE0.0166caf1.png"
+  },
+  {
+    "revision": "a3f7c7eaddfb0da816b3",
+    "url": "/wattos/static/css/main.d1bbe47e.chunk.css"
+  },
+  {
+    "revision": "84e59efe7be3476d8404",
+    "url": "/wattos/static/js/runtime~main.84e59efe.js"
+  },
+  {
+    "revision": "1a77b0e455530ac678ea",
+    "url": "/wattos/static/js/2.1a77b0e4.chunk.js"
+  },
+  {
+    "revision": "6caa575a60e0f09ad0dcb2d4aae38b86",
+    "url": "/wattos/static/media/QGbBq.6caa575a.png"
+  },
+  {
+    "revision": "2f485177f4d2565d5b8bdea04fe937b4",
+    "url": "/wattos/static/media/HBhDd.2f485177.png"
+  },
+  {
+    "revision": "ae7d404a9a88bca94af6925c86da32c4",
+    "url": "/wattos/static/media/e1bbc.ae7d404a.png"
+  },
+  {
+    "revision": "6737486c00f9381b110815fb812d7da4",
+    "url": "/wattos/static/media/FLfkh.6737486c.png"
+  },
+  {
+    "revision": "1271ddfb360a13edb7885a83a47d5879",
+    "url": "/wattos/static/media/uksPm.1271ddfb.png"
+  },
+  {
+    "revision": "555a315af869fab62216383c4200217f",
+    "url": "/wattos/static/media/rdDEP.555a315a.png"
+  },
+  {
+    "revision": "eb94be9d3690a15029ffeeef91adb1b9",
+    "url": "/wattos/static/media/LaEdm.eb94be9d.png"
+  },
+  {
+    "revision": "a3f7c7eaddfb0da816b3",
+    "url": "/wattos/static/js/main.a3f7c7ea.chunk.js"
+  },
+  {
+    "revision": "93f5c8343146120747694c8847144455",
+    "url": "/wattos/static/media/notfound.93f5c834.png"
+  },
+  {
+    "revision": "2e60bb7d2e12ae1405f2a98523aaef12",
+    "url": "/wattos/static/media/loading.2e60bb7d.png"
+  },
+  {
+    "revision": "2e45e1426cbefef2af3789cef4ef2793",
+    "url": "/wattos/static/media/failed.2e45e142.png"
+  },
+  {
+    "revision": "0c3244183526daa793ad441a573f1ca3",
+    "url": "/wattos/static/media/splash.0c324418.png"
+  },
+  {
+    "revision": "a9bcf765348c213149e81a11af050d92",
+    "url": "/wattos/static/media/aurebesh.a9bcf765.woff2"
+  },
+  {
+    "revision": "bc435f7901191d1e89c7e60498471cd4",
+    "url": "/wattos/static/media/mandalorian.bc435f79.woff2"
+  },
+  {
+    "revision": "b51af97f55891b872cf09e2d06f1c81f",
+    "url": "/wattos/static/media/darkkatarn.b51af97f.woff2"
+  },
+  {
+    "revision": "faf27ccfaba46b8d46f4a09a9ff81c26",
+    "url": "/wattos/static/media/titillium.faf27ccf.woff2"
+  },
+  {
+    "revision": "929fa32378613637ae384cf5a96eb7fc",
+    "url": "/wattos/static/media/starfield.929fa323.jpg"
+  },
+  {
+    "revision": "0d8dd22ba0b67ad77be7e80dc245da87",
+    "url": "/wattos/index.html"
+  }
+];
